6d2daaf6f6a6e93ea5fbef7288f4703a1370d0d1
Velimir <87824619+VtheSaint@users.noreply.github.com>
2023-06-25 12:19:04 +0300

    ref

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,7 +11,7 @@ const count = ref(0)
 
 <template>
   <h1>{{ msg }}</h1>
-  <h1>Output is {{ output }}</h1>
+  <h4>Output is {{ output }}</h4>
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
